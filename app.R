89b7b20b561ce47a6d642394bb08ff9167fb21b9
mklau <mk@mklau.info>
2024-07-26 14:24:50 -1000

    Integrates Type into data

--- app.R
+++ app.R
@@ -11,7 +11,8 @@ for (i in seq_along(sample_data[, "Resource"])){
     sample_data[i, "Resource"] <- x
 }
 
-sample_data <- sample_data[, c("Practice", "Mitigation", "Description", "Resource")]
+sample_data <- sample_data[, c("Type", "Practice", 
+                               "Mitigation", "Description", "Resource")]
 sample_data <- sample_data[!(grepl("Additional", sample_data[, "Mitigation"])), ]
 
 # Define UI for the Shiny app
